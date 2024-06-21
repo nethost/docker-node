137561a68448d71fbe0681000899c7da66614ada
billgo <cocobill@vip.qq.com>
2024-06-21 10:58:15 +0800

    update

--- debian-22/build.sh
+++ debian-22/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64" latest builder
+docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64" latest
+
+docker pull nethost/node:22.3-debian && docker tag nethost/node:22.3-debian registry.cn-shanghai.aliyuncs.com/bridge8/node:22.3-debian && docker push registry.cn-shanghai.aliyuncs.com/bridge8/node:22.3-debian
